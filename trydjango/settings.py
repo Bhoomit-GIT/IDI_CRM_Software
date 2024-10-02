0135c216793f8a7b8e799abe15993a3bbe4ecb58
Bhumit <rachchawarb@gmail.com>
2024-10-02 11:55:57 +0530

    Changed the django control folder name from Invoice to trydjango to avoid confusions

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'Invoice.urls'
+ROOT_URLCONF = 'trydjango.urls'
 
 TEMPLATES = [
     {
@@ -74,7 +74,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'Invoice.wsgi.application'
+WSGI_APPLICATION = 'trydjango.wsgi.application'
 
 
 # Database
